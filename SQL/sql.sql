61fc9f4d812c56924211c6a2e3e48588cf3bbb6c
Lucas-O-S <lucasoscomercial@gmail.com>
2024-10-20 16:16:27 -0300

    banco de dados

---
+++ SQL/sql.sql
@@ -0,0 +1,85 @@
+use AULADB
+go
+
+create table tbPizza(id int not null primary key identity (1,1), descricao varchar(100) not null )
+go
+
+create table tbIngredientesPizza (id int not null primary key identity (1,1),  pizzaId  int not null, descricao varchar(100) not null )
+go
+
+create or alter procedure sp_delete(
+	@id int,
+	@tabela varchar(max)
+)
+as
+begin
+	declare @sql varchar(max)
+	set @sql = 'delete ' + @tabela + ' where id = ' + cast( @id as varchar(max))
+	exec(@sql)
+end
+go
+
+create or alter procedure sp_select(
+	@id int,
+	@tabela varchar(max)
+)
+as
+begin
+	declare @sql varchar(max)
+	set @sql = 'select * from  ' + @tabela + ' where id = ' + cast( @id as varchar(max))
+	exec(@sql)
+end
+go
+
+create or alter procedure sp_list(
+	@tabela varchar(max)
+)
+as
+begin
+	declare @sql varchar(max)
+	set @sql = 'select * from  ' + @tabela
+	exec(@sql)
+end
+go
+
+create or alter procedure sp_insert_tbPizza(
+	@descricao varchar(max)
+)
+as
+begin
+	insert into tbPizza values (@descricao)
+end
+go
+
+create or alter procedure sp_insert_tbIngredientesPizza(
+	@pizzaId int,
+	@descricao varchar(max)
+	
+)
+as
+begin
+	insert into tbIngredientesPizza(pizzaId,descricao) values (@descricao,@pizzaId)
+end
+go
+
+create or alter procedure sp_update_tbPizza(
+	@id int,
+	@descricao varchar(max)
+)
+as
+begin
+	update tbPizza set descricao = @descricao where @id = id
+end
+go
+
+create or alter procedure sp_update_tbIngredientesPizza(
+	@id int,
+	@pizzaId int,
+	@descricao varchar(max)
+	
+)
+as
+begin
+	update tbIngredientesPizza set pizzaId = @pizzaId, descricao = @descricao where id = @id
+end
+go
